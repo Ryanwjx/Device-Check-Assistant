53d1c483d108523070a466b2d55868a4cb925c2b
root <root@100ask.localdomain>
2025-02-22 06:59:51 -0500

    remove useless files

--- my2_product_assist.si4project/Backup/SignalSlotCreate(6882).h
+++
@@ -1,9 +0,0 @@
-#ifndef _SIGNALSLOTCREATE_H
-#define _SIGNALSLOTCREATE_H
-
-void * CreateSignal0(void);
-void DeleteSignal0(void * tSignal);
-void ConnectSignal0Slot(void * Signal, void (*SlotFunc)(void));
-void EmitSignal0(void * Signal);
-
-#endif //_SIGNALSLOTCREATE_H
